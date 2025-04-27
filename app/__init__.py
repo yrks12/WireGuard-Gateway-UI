e7f61f0768c7b20395144ed226d6593a0d9c4b57
Yair <yair@pi>
2025-04-27 14:48:15 +0100

    Set logging level to DEBUG for more detailed logs

--- app/__init__.py
+++ app/__init__.py
@@ -63,8 +63,9 @@ def create_app(test_config=None):
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
         ))
-        file_handler.setLevel(logging.INFO)
+        file_handler.setLevel(logging.DEBUG)
         app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.DEBUG)
         app.logger.setLevel(logging.INFO)
         app.logger.info('WireGuard Gateway UI startup')
 
