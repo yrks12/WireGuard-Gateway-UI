83511e3364e0dd9eb9f3250b5f836bfd9c1d2a4e
Yair <yair@pi>
2025-04-27 14:23:44 +0100

    Update user model to use app.db instead of local SQLAlchemy instance

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
-
-db = SQLAlchemy()
+from app import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
